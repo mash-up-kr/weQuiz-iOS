f250e5dd1d7cb93db687e9fb01b4b8287ee16f02
AhnSangHoon <ansh0324@gmail.com>
2023-06-07 23:35:23 +0900

    :blub: WQSearchBar, WQTopBar 구현 (#8)
    공통으로 사용될 WQSearchBar, WQTopBar를 구현하고, Preview를 작성하였습니다.
    
    WQSearchBar는 ClearButton까지 적용되어있습니디.
    추가로 다른곳에서도 ClearButton을 추가할 수 있도록 View Extension을 작성하였습니다.

---
+++ Projects/DesignSystem/Targets/DesignSystemKit/Sources/Extensions/View+ClearButton.swift
@@ -0,0 +1,41 @@
+//
+//  View+ClearButton.swift
+//  DesignSystemUI
+//
+//  Created by AhnSangHoon on 2023/06/06.
+//  Copyright © 2023 ommaya.io. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ClearButton: ViewModifier {
+    @Binding var textFieldInput: String
+    let image: Image
+    
+    public init(textFieldInput: Binding<String>, image: Image) {
+        self._textFieldInput = textFieldInput
+        self.image = image
+    }
+    
+    public func body(content: Content) -> some View {
+        ZStack(alignment: .trailing) {
+            content
+            if !textFieldInput.isEmpty {
+                Button {
+                    textFieldInput = ""
+                } label: {
+                    Image(Icon.Close.fillBlack)
+                }
+                .padding(.trailing, 8)
+            }
+        }
+    }
+}
+
+public extension View {
+    func clearButton(_ button: ClearButton) -> some View {
+        modifier(
+            button
+        )
+    }
+}
