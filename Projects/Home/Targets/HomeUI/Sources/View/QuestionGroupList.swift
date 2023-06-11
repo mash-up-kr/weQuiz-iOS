a8841c181434a331212ead50188e6174544875ed
최원석 <cws653@naver.com>
2023-06-11 17:01:09 +0900

    :sparkles: Home뷰 기본 틀 구현 (#9, #10, #11, #12)
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 세부 내용을 아랫줄에 작성
    - 현재 디자인 미확정 및 Component 미확정에 따라 향후 디테일하게 수정 예정
    - Home, 친구랭킹 리스트, 문제리스트, 문제만들기 페이지 구현
    
    ################
    # 추가적인 세부 내용을 아랫줄에 작성
    
    ################
    # :tada: = 프로젝트 시작
    # :sparkles: 
    # :bulb: = update 코드 및 UI 업데이트, 기능 구현 시
    # :rocket: = release 배포 시
    # :recycle: = refactor 리팩토링 시 (기능은 같은데 코드가 달라지는경우)
    # :memo: = 문서 작성 시
    # :art: = 디자인 시스템 관련 내용 (Color / Asset)
    # :hammer: = 테스트 코드 작성
    ################

---
+++ Projects/Home/Targets/HomeUI/Sources/View/QuestionGroupList.swift
@@ -0,0 +1,76 @@
+//
+//  QuestionsList.swift
+//  HomeUI
+//
+//  Created by 최원석 on 2023/05/28.
+//  Copyright © 2023 ommaya.io. All rights reserved.
+//
+
+import SwiftUI
+
+struct QuestionGroupList: View {
+    @Binding var questions: [QuestionGroup]
+    @State private var isEditing = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: ListHeader(questions: $questions, isEditing: $isEditing)) {
+                    ForEach(questions) { question in
+                        QuestionGroupRow(questionGroup: question)
+                    }
+                    .onDelete(perform: deleteItems)
+                    .headerProminence(.increased)
+                    .listStyle(.plain)
+                }
+            }
+            .animation(.default)
+            .environment(\.editMode, .constant(isEditing ? EditMode.active : EditMode.inactive))
+        }
+        .navigationBarTitle("문제 리스트")
+        .preferredColorScheme(.dark)
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        questions.remove(atOffsets: offsets)
+    }
+}
+
+struct ListHeader: View {
+    @Binding var questions: [QuestionGroup]
+    @Binding var isEditing: Bool
+    
+    var body: some View {
+        HStack {
+            Text("전체\(questions.count)")
+                .font(.system(size: 14))
+                .font(.title)
+            Spacer()
+            CustomEditButton(isEditing: $isEditing)
+        }
+    }
+}
+
+struct CustomEditButton: View {
+    @Binding var isEditing: Bool
+    
+    private var title: String {
+        return isEditing ? "삭제" : "편집"
+    }
+    
+    var body: some View {
+        Button(action: {
+            isEditing.toggle()
+        }) {
+            Text(title)
+                .font(.system(size: 14))
+        }
+        .animation(.default)
+    }
+}
+
+//struct QuestionsList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QuestionsList(question: questionsSamlple[0])
+//    }
+//}
---
+++ Projects/Home/Targets/HomeUI/Sources/ViewModel/HomeViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewModel.swift
+//  HomeUI
+//
+//  Created by 최원석 on 2023/05/28.
+//  Copyright © 2023 ommaya.io. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+
+public class HomeViewModel: ObservableObject {
+    @Published var questionGroups: [QuestionGroup]
+    @Published var friendsRank: [Friend]
+    @Published var profile: Profile
+    @Published var explainContents: Explain
+    
+    public init() {
+        self.questionGroups = questionsSamlple
+        self.friendsRank = friendsRankSample
+//        self.friendsRank = []
+        self.profile = profileSamlple
+        self.explainContents = explainSample
+    }
+}
