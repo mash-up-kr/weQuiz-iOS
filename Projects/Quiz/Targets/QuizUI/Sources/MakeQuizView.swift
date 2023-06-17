18202f9284a468488094018432f09a345e382482
sohyeon <sohyun1018@gmail.com>
2023-06-18 00:44:34 +0900

    :bulb: 문제 만들기 View UI (#14)

--- Projects/Quiz/Targets/QuizUI/Sources/MakeQuizView.swift
+++ Projects/Quiz/Targets/QuizUI/Sources/MakeQuizView.swift
@@ -7,7 +7,7 @@ public struct MakeQuizView: View {
     
     let quizNameLimit: Int = 38
     @State var quizName: String = ""
-    @State var questionItem: [String] = ["1","2","3","4"]
+    @State var questionItem: [String] = ["문제 1","문제 2","문제 3","문제 4"]
 
     public var body: some View {
         
@@ -16,11 +16,14 @@ public struct MakeQuizView: View {
                 VStack {
                     VStack {
                         TextField("", text: $quizName, prompt: Text("제목없는 시험지").foregroundColor(Color.designSystem(.g4)))
+                            .frame(height: 34)
                             .onReceive(Just(quizName)) { _ in
                                 limitQuizName(quizNameLimit)
                             }
                             .font(.pretendard(.medium, size: ._24))
                             .foregroundColor(Color.designSystem(.g4))
+                            .padding(EdgeInsets(top: 24, leading: 20, bottom: 0, trailing: 20))
+                        
                         List {
                             Section(content: {
                                 ForEach(questionItem, id: \.self) { item in
@@ -29,32 +32,61 @@ public struct MakeQuizView: View {
                                         .listRowSeparator(.hidden)
                                         .listRowInsets(EdgeInsets())
                                         .listRowBackground(Color.designSystem(.g9))
+                                        .padding(.bottom, 16)
                                 }
                                 .onMove(perform: moveListItem)
                             }, footer: {
-                                Button("질문 추가", action: {
+                                
+                                Button(action: {
                                     self.questionItem.append("질!문!")
-                                })
-                                .drawingGroup()
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 56)
-                                .background(.red)
+                                }) {
+                                    HStack(alignment: .center, spacing: 7, content: {
+                                        
+                                        Text(".")
+                                            .frame(width: 16.5, height: 16.5)
+                                            .background(Color.white)
+                                        
+                                        Text("질문 추가")
+                                            .font(.pretendard(.bold, size: ._16))
+                                            .foregroundColor(Color.designSystem(.g1))
+                                            
+                                    })
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 56)
+                                    .background(.red)
+                                    .cornerRadius(16)
+                                    
+                                }
+                                .background(
+                                    Color.designSystem(.g9)
+                                )
+                                .listRowInsets(EdgeInsets())
+                                
                             })
                         }
                         .listStyle(.plain)
+                        .padding(EdgeInsets(top: 30, leading: 20, bottom: 0, trailing: 20))
                     }
                     
                     Spacer()
-                    
-                    Button("시험지 만들기", action: {
-                        print("make quiz")
-                    })
-                    .frame(maxWidth: .infinity)
+
+                    WQButton(
+                      style: .single(
+                          .init(
+                              title: "시험지 완성하기",
+                              action: {
+                                  print("make quiz")
+                              }
+                          )
+                      )
+                    )
                     .frame(height: 52)
+                    
                 }
+                
+                
             }
             .frame(maxWidth: .infinity)
-            .padding(EdgeInsets(top: 24, leading: 20, bottom: 24, trailing: 20))
             .ignoresSafeArea(.keyboard, edges: .bottom)
             .navigationBarTitle(Text("문제 만들기"),
                                     displayMode: .inline)
