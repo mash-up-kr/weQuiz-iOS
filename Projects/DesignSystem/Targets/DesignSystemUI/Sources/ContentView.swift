ce240afa571c86ca5046a72b7509e76be945ef77
AhnSangHoon <ansh0324@gmail.com>
2023-06-27 00:17:07 +0900

    :bulb: WQToast 구현 (#8)
    Toast Component 구현

--- Projects/DesignSystem/Targets/DesignSystemUI/Sources/ContentView.swift
+++ Projects/DesignSystem/Targets/DesignSystemUI/Sources/ContentView.swift
@@ -14,6 +14,7 @@ public struct ContentView: View {
         case color
         case component
         case modal
+        case toast
     }
 
     public var body: some View {
@@ -25,6 +26,7 @@ public struct ContentView: View {
                 NavigationLink("Color", value: NavigationType.color)
                 NavigationLink("Component", value: NavigationType.component)
                 NavigationLink("Modal", value: NavigationType.modal)
+                NavigationLink("Toast", value: NavigationType.toast)
             }
             .navigationDestination(for: NavigationType.self) {
                 switch $0 {
@@ -38,6 +40,8 @@ public struct ContentView: View {
                     ComponentPreview()
                 case .modal:
                     WQModalPreview()
+                case .toast:
+                    WQToastPreview()
                 }
             }
             .navigationTitle("WeQuiz DesignSystem")
