51f2baaa9ec58ed16bb6d267cc33e12f54ea6576
AhnSangHoon <ansh0324@gmail.com>
2023-06-28 00:14:27 +0900

    :bulb: VerificationCodeInputView 구현

---
+++ Projects/Authentication/Targets/AuthenticationUI/Sources/SignUp/VerificationCodeInputView.swift
@@ -0,0 +1,52 @@
+//
+//  VerificationCodeInputView.swift
+//  Authentication
+//
+//  Created by AhnSangHoon on 2023/06/28.
+//  Copyright © 2023 ommaya.io. All rights reserved.
+//
+
+import SwiftUI
+
+import DesignSystemKit
+
+struct VerificationCodeInputView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            WQTopBar(style: .navigation(.init(title: "")))
+            VStack(alignment: .leading, spacing: .zero) {
+                Text("휴대폰 인증번호를\n입력해 주세요")
+                    .font(.pretendard(.bold, size: ._24))
+                    .foregroundColor(.white)
+                Spacer()
+                    .frame(height: 36)
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("인증번호")
+                        .font(.pretendard(.medium, size: ._12))
+                        .foregroundColor(.designSystem(.g2))
+                    // 임시 컴포넌트
+                    Text("임시 VerificationCode Input")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 32)
+                        .background(Color.designSystem(.p1))
+                    //
+                }
+            }
+            .padding(
+                .init(
+                    top: 20,
+                    leading: 20,
+                    bottom: .zero,
+                    trailing: 20
+                )
+            )
+            Spacer()
+        }
+    }
+}
+
+struct VerificationCodeInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        VerificationCodeInputView()
+    }
+}
