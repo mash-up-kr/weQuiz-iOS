1b53a1048355efb17b18dd41c423b8b99cfe3541
sohyeon <sohyun1018@gmail.com>
2023-07-15 16:49:58 +0900

    :bulb: 문제풀기 - 문제 풀기 뷰 추가 (#57)

--- Projects/Quiz/Targets/QuizUI/Sources/Solve/SolveQuizView.swift
+++ Projects/Quiz/Targets/QuizUI/Sources/Solve/SolveQuizView.swift
@@ -13,21 +13,62 @@ public struct SolveQuizView: View {
     
     public init() {}
     
+    @State var nowCount: Int = 1
+    var allCount: Int = 10
+    
     public var body: some View {
-        VStack {
-            WQTopBar(style: .navigationWithButtons(
-                .init(title: "",
-                      bttons: [
-                        .init(icon: Icon.Siren.mono, action: {
-                    print("신고하기 버튼 클릭")
-                })
-                ])
-            ))
+        
+        ZStack {
+            VStack {
+                WQTopBar(style: .navigationWithButtons(
+                    .init(title: "",
+                          bttons: [
+                            .init(icon: Icon.Siren.mono, action: {
+                        print("신고하기 버튼 클릭")
+                    })
+                    ])
+                ))
+                
+                WQGradientProgressBar(
+                    standard: allCount, // progress의 총 길이
+                    current: $nowCount // progress의 현재 값
+                )
+                
+                tooltip(nowCount, allCount)
+                
+                
+                Spacer()
+            }
+            .background(Color.designSystem(.g9))
             
-            tooltip(1, 5)
-            
-            Spacer()
+            VStack(alignment: .center) {
+                
+                Text("\(nowCount)")
+                    .font(.pretendard(.bold, size: ._20))
+                    .foregroundColor(Color.designSystem(.g1))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(height: 26)
+                Text("문제 타이틀")
+                    .font(.pretendard(.medium, size: ._24))
+                    .foregroundColor(Color.designSystem(.g1))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(height: 34)
+                Text("답 N개")
+                    .font(.pretendard(.bold, size: ._16))
+                    .foregroundStyle(
+                        DesignSystemKit.Gradient.gradientS1.linearGradient
+                    )
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .frame(height: 24)
+                
+                
+                ForEach(0..<5) { item in
+                    answerView()
+                }
+            }
+            .padding(.horizontal, 20)
         }
+        
         
     }
     
@@ -40,6 +81,16 @@ public struct SolveQuizView: View {
             .background(Color.designSystem(.g8))
             .cornerRadius(16)
             .frame(height: 32)
+    }
+    
+    private func answerView() -> some View {
+        Text("{질문답변}")
+            .font(.pretendard(.bold, size: ._18))
+            .foregroundColor(Color.designSystem(.g2))
+            .frame(maxWidth: .infinity, alignment: .center)
+            .frame(height: 58)
+            .background(Color.designSystem(.g8))
+            .cornerRadius(16)
     }
 }
 
