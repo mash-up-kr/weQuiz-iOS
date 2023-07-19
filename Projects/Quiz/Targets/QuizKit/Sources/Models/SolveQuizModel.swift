0153128ac6f6d5817f8a775e764cc38dcdd16235
sohyeon <sohyun1018@gmail.com>
2023-07-19 23:35:02 +0900

    :bulb: 문제 풀기 - 문제 풀기 모델 추가 (#57)

---
+++ Projects/Quiz/Targets/QuizKit/Sources/Models/SolveQuizModel.swift
@@ -0,0 +1,44 @@
+//
+//  SolveQuizModel.swift
+//  QuizKit
+//
+//  Created by 박소현 on 2023/07/19.
+//  Copyright © 2023 ommaya.io. All rights reserved.
+//
+
+import Foundation
+
+public struct SolveQuizModel {
+    public var title: String
+    public var questions: [SolveQuestionModel]
+    
+    public init(title: String, questions: [SolveQuestionModel]) {
+        self.title = ""
+        self.questions = questions
+    }
+}
+
+public struct SolveQuestionModel: Identifiable {
+    public var id: UUID = UUID()
+    public var title: String
+    public var answerCount: Int
+    public var answers: [SolveAnswerModel]
+    
+    public init(title: String, answerCount: Int, answers: [SolveAnswerModel]) {
+        self.title = title
+        self.answerCount = answerCount
+        self.answers = answers
+    }
+}
+
+public struct SolveAnswerModel {
+    public var answer: String
+    public var order: Int
+    public var isCorrect: Bool
+    
+    public init(answer: String, order: Int, isCorrect: Bool) {
+        self.answer = answer
+        self.order = order
+        self.isCorrect = isCorrect
+    }
+}
