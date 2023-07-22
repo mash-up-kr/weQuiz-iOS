eca902baeefe3b169a1c6307784d00f93be225dd
sohyeon <sohyun1018@gmail.com>
2023-07-22 22:02:53 +0900

    :bulb: 문제 결과 - QuizResultModel 추가 (#73)

---
+++ Projects/Quiz/Targets/QuizKit/Sources/Models/QuizResultModel.swift
@@ -0,0 +1,30 @@
+//
+//  QuizResultModel.swift
+//  QuizKit
+//
+//  Created by 박소현 on 2023/07/22.
+//  Copyright © 2023 ommaya.io. All rights reserved.
+//
+
+import Foundation
+
+public struct QuizResultModel {
+    public let myScore: Int
+    public let myNickname: String
+    public let friendNickname: String
+    public let ranking: [RankUserModel]?
+    
+    public init(myScore: Int, myNickname: String, friendNickname: String, ranking: [RankUserModel]? = nil) {
+        self.myScore = myScore
+        self.myNickname = myNickname
+        self.friendNickname = friendNickname
+        self.ranking = ranking
+    }
+}
+
+public struct RankUserModel {
+    public let id: Int
+    public let name: String
+    public let score: Int
+    
+}
