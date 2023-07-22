95f6cbfc2e6667e5ffc1b29fe4b5f50ebcc3a4bf
sohyeon <sohyun1018@gmail.com>
2023-07-22 22:29:33 +0900

    :bulb: 문제 결과 - 문제 풀이 결과 뷰에 모델 추가 (#73)

--- Projects/Quiz/Targets/QuizUI/Sources/QuizResult/QuizResultView.swift
+++ Projects/Quiz/Targets/QuizUI/Sources/QuizResult/QuizResultView.swift
@@ -7,11 +7,17 @@
 //
 
 import SwiftUI
+import QuizKit
 import DesignSystemKit
+
 
 public struct QuizResultView: View {
     
-    public init() {}
+    public let model: QuizResultModel
+    
+    public init(_ model: QuizResultModel) {
+        self.model = model
+    }
     
     @State private var isSharePresented = false
     
@@ -29,10 +35,12 @@ public struct QuizResultView: View {
                         .aspectRatio(1.0, contentMode: .fit)
                     
                     rankingView()
+                        .hidden(model.ranking == nil)
                 }
             }
             .padding(.top, 58)
             .padding(.bottom, 128)
+            .disabled(model.ranking == nil)
             
             VStack {
                 topBar()
@@ -74,7 +82,7 @@ extension QuizResultView {
     private func socreView() -> some View {
         HStack(alignment: .center, spacing: 6) {
             // TODO: - size 68로 수정
-            Text("20")
+            Text("\(model.myScore)")
                 .foregroundStyle(DesignSystemKit.Gradient.gradientS1.linearGradient)
                 .font(.pretendard(.bold, size: ._48))
                 .frame(height: 54)
@@ -87,12 +95,12 @@ extension QuizResultView {
     
     private func descriptionView() -> some View {
         VStack(alignment: .center, spacing: 0) {
-            Text("name1과 name2는")
+            Text("\(model.myNickname)과 \(model.friendNickname)는")
                 .foregroundColor(Color.designSystem(.g4))
                 .font(.pretendard(.regular, size: ._18))
                 .frame(height: 26)
             
-            Text("옆집보다 못한 사이")
+            Text("\(getScoreDescription(model.myScore))")
                 .foregroundColor(Color.designSystem(.g1))
                 .font(.pretendard(.bold, size: ._28))
                 .frame(height: 38)
@@ -113,10 +121,13 @@ extension QuizResultView {
                 .frame(height: 8)
             
             // TODO: - 데이터 받아서 수정
-            ForEach(0..<3) {_ in
-                QuizResultRankCell()
+            if let ranking = model.ranking {
+                ForEach(ranking, id: \.id) { user in
+                    QuizResultRankView(user)
+                }
+                .padding(.horizontal, 20)
             }
-            .padding(.horizontal, 20)
+            
         }
     }
     
@@ -136,5 +147,36 @@ extension QuizResultView {
         }
         .frame(height: 56)
         .background(Color.designSystem(.g9))
+    }
+    
+    // TODO: - 통신 추가하면 모델 만드는 쪽으로 이동
+    private func getScoreDescription(_ score: Int) -> String {
+        switch score {
+        case 90...100:
+            return "영혼을 공유한 사이"
+        case 70..<90:
+            return "아직 거리가 있는 친구"
+        case 50..<70:
+            return "거리가 머네요"
+        case 30..<50:
+            return "그냥 얼굴만 아는 사이"
+        default:
+            return "엽짚보다도 못한 사이"
+        }
+    }
+}
+struct QuizResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Ranking 있는 경우
+        QuizResultView(
+            .init(myScore: 100, myNickname: "두루미", friendNickname: "두더지",
+                  ranking: [RankUserModel(id: 1111, name: "감자", rank: 1, score: 100),
+                            RankUserModel(id: 2434, name: "족제비", rank: 2, score: 98),
+                            RankUserModel(id: 3555, name: "원숭이", rank: 3, score: 95),
+                            RankUserModel(id: 2335, name: "메타몽", rank: 4, score: 77),
+                            RankUserModel(id: 3252, name: "강아지", rank: 5, score: 60),
+                            RankUserModel(id: 4556, name: "오리", rank: 6, score: 51)]))
+        // 랭킹 없는 경우
+//        QuizResultView(.init(myScore: 100, myNickname: "두루미", friendNickname: "두더지"))
     }
 }
