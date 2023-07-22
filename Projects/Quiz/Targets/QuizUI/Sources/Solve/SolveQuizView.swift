53757332309eedefdb4fbc140ad19fdb76da3f82
sohyeon <sohyun1018@gmail.com>
2023-07-22 15:19:12 +0900

    :recycle: 문제 풀기 - nowCount -> nowIndex로 변수명 수정  (#57)

--- Projects/Quiz/Targets/QuizUI/Sources/Solve/SolveQuizView.swift
+++ Projects/Quiz/Targets/QuizUI/Sources/Solve/SolveQuizView.swift
@@ -15,7 +15,7 @@ public struct SolveQuizView: View {
     public init() {}
     
     @ObservedObject var viewModel = SolveQuizViewModel()
-    @State var nowCount: Int = 0
+    @State var nowIndex: Int = 0
     @State var selectedCount: Int = 0
     
     public var body: some View {
@@ -33,7 +33,7 @@ public struct SolveQuizView: View {
                 
                 WQGradientProgressBar(
                     standard: viewModel.quiz.questions.count,
-                    current: $nowCount
+                    current: $nowIndex
                 )
                 
                 tooltip()
@@ -44,17 +44,17 @@ public struct SolveQuizView: View {
             
             VStack(alignment: .center) {
                 
-                Text("\(nowCount + 1)")
+                Text("\(nowIndex + 1)")
                     .font(.pretendard(.bold, size: ._20))
                     .foregroundColor(Color.designSystem(.g1))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .frame(height: 26)
-                Text(viewModel.quiz.questions[nowCount].title)
+                Text(viewModel.quiz.questions[nowIndex].title)
                     .font(.pretendard(.medium, size: ._24))
                     .foregroundColor(Color.designSystem(.g1))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .frame(height: 34)
-                Text("답 \(viewModel.quiz.questions[nowCount].answerCount)개")
+                Text("답 \(viewModel.quiz.questions[nowIndex].answerCount)개")
                     .font(.pretendard(.bold, size: ._16))
                     .foregroundStyle(
                         DesignSystemKit.Gradient.gradientS1.linearGradient
@@ -62,11 +62,11 @@ public struct SolveQuizView: View {
                     .frame(maxWidth: .infinity, alignment: .trailing)
                     .frame(height: 24)
                 
-                ForEach($viewModel.quiz.questions[nowCount].answers, id: \.id) { answer in
+                ForEach($viewModel.quiz.questions[nowIndex].answers, id: \.id) { answer in
                     SolveQuizAnswerView(answer)
                         .onChange(of: answer.isSelected.wrappedValue, perform: { isSelected in
                             selectedCount = (isSelected == true) ? selectedCount + 1 : selectedCount - 1
-                            if selectedCount == viewModel.quiz.questions[nowCount].answerCount {
+                            if selectedCount == viewModel.quiz.questions[nowIndex].answerCount {
                                 onNextQuestion()
                             }
                         })
@@ -79,8 +79,8 @@ public struct SolveQuizView: View {
     
     private func onNextQuestion() {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2) {
-            if nowCount + 1 < viewModel.quiz.questions.count {
-                nowCount += 1
+            if nowIndex + 1 < viewModel.quiz.questions.count {
+                nowIndex += 1
                 selectedCount = 0
             } else {
                 // TODO: - 문제 다 풀었을 때
@@ -89,7 +89,7 @@ public struct SolveQuizView: View {
     }
     
     private func tooltip() -> some View {
-        Text("\(viewModel.quiz.questions.count)문제 중 \(nowCount + 1)번째 문제")
+        Text("\(viewModel.quiz.questions.count)문제 중 \(nowIndex + 1)번째 문제")
             .font(.pretendard(.bold, size: ._14))
             .foregroundColor(.designSystem(.g4))
             .padding(.horizontal, 16)
