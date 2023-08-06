64d0e59cd7b95e8288f91ce20c5fde126032ec69
AhnSangHoon <ansh0324@gmail.com>
2023-08-06 15:10:20 +0900

    :recycle: dynamic link 생성 시 description 추가(#111)
    이미지는 추후에 서버작업이 완료되면 진행

--- Projects/WeQuiz/Sources/Core/DynamicLinks.swift
+++ Projects/WeQuiz/Sources/Core/DynamicLinks.swift
@@ -24,11 +24,12 @@ enum DynamicLinks {
             }
         }
         
-        var metaTagtitle: String {
-            switch self {
-            case .solve: return "친구가 만든 찐친고사에 도전해보세요!"
-            case .result: return "찐친고사 결과를 확인해보세요!"
-            }
+        var metaTagTitle: String {
+            "진정한 친구들만 통과할 수 있는 찐친고사"
+        }
+        
+        var metaTagDescription: String {
+            "너 나 알아? WeQuiz 우정테스트"
         }
     }
     
@@ -58,7 +59,8 @@ enum DynamicLinks {
         builder?.androidParameters = DynamicLinkAndroidParameters(packageName: "team.ommaya.wequiz.android")
         
         builder?.socialMetaTagParameters = DynamicLinkSocialMetaTagParameters()
-        builder?.socialMetaTagParameters?.title = type.metaTagtitle
+        builder?.socialMetaTagParameters?.title = type.metaTagTitle
+        builder?.socialMetaTagParameters?.descriptionText = type.metaTagDescription
         
         builder?.shorten(completion: { url, options, error in
             if let error = error  {
